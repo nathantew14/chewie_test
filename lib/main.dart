eef492994d107f4a7070657d4ad2bad74104ced6
Nathan Tew <nathantew14@gmail.com>
2021-12-09 02:26:03 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import './chewie_list_item.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'videos/IntroVideo.mp4',
+            ),
+            looping: false,
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'videos/sample.mp4',
+            ),
+            looping: false,
+          ),
+        ],
+      ),
+    );
+  }
+}
